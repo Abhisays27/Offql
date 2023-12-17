d57b85f62c428f6f791aab20b83cbc88e8bf6723
abhishek <abhishek.nt94@gmail.com>
2023-12-17 10:07:38 -0800

    Desktop page done

---
+++ src/components/Mainhead.jsx
@@ -0,0 +1,118 @@
+import React from "react";
+import "./mainHead.css";
+import first from '../assests/first.jpeg'
+import second from '../assests/second.jpeg'
+import third from '../assests/third.jpeg'
+import fourth from '../assests/fourth.jpeg'
+import five from '../assests/five.jpeg'
+import six from '../assests/six.jpeg'
+const Mainhead = () => {
+  return (
+    <div className="all">
+    <div className="main">
+      <div className="firstline">
+        <div className="text-fl">
+          <h1>Teamwork Made Easier</h1>
+          <p>
+            Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+            consectetur, adipisci velit Neque porro quisquam est qui dolorem
+            ipsum quia dolor sit amet, consectetur, adipisci velit
+          </p>
+        </div>
+        <img src={first} alt="" className="img1vec"/>
+      </div>
+
+      <div className="firstline">
+      <img src={second} alt="" className="img1vec"/>
+        <div className="text-fl">
+        <h1>Budget Friendly</h1>
+          <p>
+            Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+            consectetur, adipisci velit Neque porro quisquam est qui dolorem
+            ipsum quia dolor sit amet, consectetur, adipisci velit
+          </p>
+          
+        </div>
+        
+      </div>
+
+      <div className="firstline">
+     
+        <div className="text-fl">
+        <h1>Higher sales productivity from all<br/> your teams</h1>
+          <p>
+            Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+            consectetur, adipisci velit Neque porro quisquam est qui dolorem
+            ipsum quia dolor sit amet, consectetur, adipisci velit
+          </p>
+          
+        </div>
+        <img src={third} alt="" className="img1vec"/>
+        
+      </div>
+
+      <div className="firstline">
+      <img src={fourth} alt="" className="img1vec"/>
+      <svg xmlns="http://www.w3.org/2000/svg" width="150" height="127" viewBox="0 0 150 127" fill="none">
+  <path d="M106.705 6.39315L11.426 48.4633C7.78648 50.0703 6.14627 54.3469 7.76246 58.0153L33.5326 116.508C35.1487 120.176 39.4093 121.847 43.0489 120.24L138.328 78.1703C141.967 76.5633 143.607 72.2866 141.991 68.6182L116.221 10.1256C114.605 6.45719 110.344 4.78613 106.705 6.39315Z" stroke="#F9C62E" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M11.936 67.6075L120.47 19.5248" stroke="#F9C62E" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M116.938 41.8719L99.4377 49.5991L110.628 74.9981L128.128 67.2709L116.938 41.8719Z" stroke="#F9C62E" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M108.017 51.9438C110.631 46.2831 117.798 47.6746 120.17 62.4375L126.577 63.7508L128.098 67.2142L110.617 74.9481L108.57 70.3189L114.733 65.7766C114.733 65.7766 105.561 57.2807 108.017 51.9438Z" stroke="#F9C62E" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M76.9766 94.9978C76.9766 94.9978 90.2277 91.5646 85.725 89.9155C81.2223 88.2663 79.9324 85.7234 83.141 84.8141C86.3497 83.9048 93.422 88.7004 91.9712 85.0053C90.5205 81.3102 93.5162 83.1639 94.0275 83.2925C94.5387 83.4212 97.943 80.8493 98.2474 81.2491C98.5519 81.649 98.6274 82.9734 110.548 77.7053" stroke="#F9C62E" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M24.6743 76.8376L77.823 53.2871" stroke="#F9C62E" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M27.3329 82.8889L58.6395 69.0145" stroke="#F9C62E" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M32.0299 93.5636L75.6987 74.2156" stroke="#F9C62E" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M35.0655 48.1074L88.3592 24.4848" stroke="#F9C62E" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+        <div className="text-fl">
+          <h1>Grow as Your Buisness Grows</h1>
+          <p>
+            Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+            consectetur, adipisci velit Neque porro quisquam est qui dolorem
+            ipsum quia dolor sit amet, consectetur, adipisci velit
+          </p>
+        </div>
+        
+      </div>
+
+      <div className="firstline">
+        <div className="text-fl">
+          <h1>From leads to successful sales</h1>
+          <p>
+            Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+            consectetur, adipisci velit Neque porro quisquam est qui dolorem
+            ipsum quia dolor sit amet, consectetur, adipisci velit
+          </p>
+        </div>
+        <img src={five} alt="" className="img1vec"/>
+      </div>
+<div className="lastpara">
+      <div className="firstline">
+      <img src={six} alt="" className="img1vec"/>
+        <div className="text-fl">
+            <div className="lastpara">
+          <h1 >Best Support which you desire for</h1>
+          <p >
+            Neque porro quisquam est qui dolorem ipsum quia dolor sit amet,
+            consectetur, adipisci velit Neque porro quisquam est qui dolorem
+            ipsum quia dolor sit amet, consectetur, adipisci velit
+          </p>
+          </div>
+        </div>
+        
+      </div>
+      </div>
+
+      </div>
+<div className="connect">
+<svg xmlns="http://www.w3.org/2000/svg" width="1030" height="1870" viewBox="0 0 678 2147" fill="none">
+  <path d="M677 1C677 1 -41.3792 156.669 3.00007 439.5C38.2681 664.265 615 578.985 615 806.5C615 1057.5 17.7926 958.848 7.50018 1212C-3.51735 1482.99 577.689 1421.29 578.5 1692.5C579.314 1964.56 122 2146 122 2146" stroke="#407BFF" stroke-width="2" stroke-dasharray="16 16"/>
+</svg>
+</div>
+      
+    </div>
+    
+  );
+};
+
+export default Mainhead;
